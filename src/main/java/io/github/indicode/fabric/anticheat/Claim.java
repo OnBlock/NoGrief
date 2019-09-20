10d1549ae226b54c5605b5ab4686685afca396db
Indigo A <indigoa.coding@gmail.com>
2019-09-19 23:47:35 -0700

    Basic claim class with nbt writing

---
+++ src/main/java/io/github/indicode/fabric/anticheat/Claim.java
@@ -0,0 +1,88 @@
+package io.github.indicode.fabric.anticheat;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * @author Indigo Amann
+ */
+public class Claim {
+    public BlockPos min, max;
+    public Map<UUID, ClaimPermissions> permssionsMap = new HashMap();
+    public ClaimSettings settings = new ClaimSettings();
+
+    public CompoundTag toTag() {
+        CompoundTag tag =  new CompoundTag();
+        {
+            CompoundTag pos = new CompoundTag();
+            pos.putInt("minX", min.getX());
+            pos.putInt("minY", min.getY());
+            pos.putInt("minZ", min.getZ());
+            pos.putInt("maxX", max.getX());
+            pos.putInt("maxY", max.getY());
+            pos.putInt("maxZ", max.getZ());
+            tag.put("position", pos);
+        }
+        {
+            tag.put("settings", settings.toTag());
+            CompoundTag permissions = new CompoundTag();
+            permssionsMap.forEach((id, perms) -> permissions.put(id.toString(), perms.toTag()));
+            tag.put("permissions", permissions);
+        }
+        return tag;
+    }
+    public void fromTag(CompoundTag tag) {
+        {
+            CompoundTag pos = tag.getCompound("position");
+            int minX = pos.getInt("minX");
+            int minY = pos.getInt("minY");
+            int minZ = pos.getInt("minZ");
+            int maxX = pos.getInt("maxX");
+            int maxY = pos.getInt("maxY");
+            int maxZ = pos.getInt("maxZ");
+            this.min = new BlockPos(minX, minY, minZ);
+            this.max = new BlockPos(maxX, maxY, maxZ);
+        }
+        {
+            this.settings = new ClaimSettings(tag.getCompound("settings"));
+            this.permssionsMap = new HashMap<>();
+            CompoundTag permissions = new CompoundTag();
+            permissions.getKeys().forEach(key -> permssionsMap.put(UUID.fromString(key), new ClaimPermissions(permissions.getCompound(key))));
+        }
+    }
+
+    public static class ClaimPermissions {
+        public ClaimPermissions(CompoundTag tag) {
+            fromTag(tag);
+        }
+        public ClaimPermissions() {
+        }
+        public CompoundTag toTag() {
+            CompoundTag tag =  new CompoundTag();
+            return tag;
+        }
+        public void fromTag(CompoundTag tag) {
+
+        }
+    }
+    public static class ClaimSettings {
+        public ClaimSettings(CompoundTag tag) {
+            fromTag(tag);
+        }
+        public ClaimSettings() {
+        }
+        public CompoundTag toTag() {
+            CompoundTag tag =  new CompoundTag();
+            return tag;
+        }
+        public void fromTag(CompoundTag tag) {
+
+        }
+    }
+}
