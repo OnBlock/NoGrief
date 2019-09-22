c24e4e78d28dfc2a96738f89f85cc3fbebb08344
Indigo A <indigoa.coding@gmail.com>
2019-09-21 23:39:31 -0700

    Creating claims with comand

--- src/main/java/io/github/indicode/fabric/itsmine/ClaimCommand.java
+++ src/main/java/io/github/indicode/fabric/itsmine/ClaimCommand.java
@@ -1,9 +1,17 @@
 package io.github.indicode.fabric.itsmine;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import net.minecraft.command.arguments.BlockPosArgumentType;
+import net.minecraft.command.arguments.PosArgument;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.UUID;
 
 /**
  * @author Indigo Amann
@@ -11,5 +19,26 @@ import net.minecraft.server.command.ServerCommandSource;
 public class ClaimCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         LiteralArgumentBuilder<ServerCommandSource> command = CommandManager.literal("claim");
+        {
+            LiteralArgumentBuilder<ServerCommandSource> create = CommandManager.literal("create");
+            ArgumentBuilder name = CommandManager.argument("name", StringArgumentType.word());
+            ArgumentBuilder min = CommandManager.argument("min", BlockPosArgumentType.blockPos());
+            RequiredArgumentBuilder<ServerCommandSource, PosArgument> max = CommandManager.argument("max", BlockPosArgumentType.blockPos());
+            max.executes(context -> createClaim(
+                    StringArgumentType.getString(context, "name"),
+                    context.getSource().getPlayer().getGameProfile().getId(),
+                    BlockPosArgumentType.getBlockPos(context, "min"),
+                    BlockPosArgumentType.getBlockPos(context, "max")
+            ));
+            min.then(max);
+            name.then(min);
+            create.then(name);
+            command.then(create);
+        }
+        dispatcher.register(command);
+    }
+    private static int createClaim(String name, UUID owner, BlockPos min, BlockPos max) {
+        ClaimManager.INSTANCE.addClaim(new Claim(name, owner, min, max));
+        return 0;
     }
 }
