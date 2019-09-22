3216c2d5e6b3ae60f3cd99ea5b31566a318117fd
Indigo A <indigoa.coding@gmail.com>
2019-09-22 00:25:07 -0700

    Showing the outline of a claim

--- src/main/java/io/github/indicode/fabric/itsmine/ClaimCommand.java
+++ src/main/java/io/github/indicode/fabric/itsmine/ClaimCommand.java
@@ -5,10 +5,16 @@ import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.ArgumentBuilder;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import io.github.indicode.fabric.itsmine.mixin.BlockUpdatePacketMixin;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.network.packet.BlockUpdateS2CPacket;
 import net.minecraft.command.arguments.BlockPosArgumentType;
 import net.minecraft.command.arguments.PosArgument;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.BlockPos;
 
 import java.util.UUID;
@@ -35,10 +41,47 @@ public class ClaimCommand {
             create.then(name);
             command.then(create);
         }
+        {
+            LiteralArgumentBuilder<ServerCommandSource> show = CommandManager.literal("show");
+            show.executes(context -> showClaim(context.getSource().getPlayer(), ClaimManager.INSTANCE.getClaimAt(context.getSource().getPlayer().getBlockPos())));
+            RequiredArgumentBuilder<ServerCommandSource, String> name = CommandManager.argument("name", StringArgumentType.word());
+            name.executes(context -> showClaim(context.getSource().getPlayer(), ClaimManager.INSTANCE.claimsByName.get(StringArgumentType.getString(context, "name"))));
+            show.then(name);
+            command.then(show);
+        }
         dispatcher.register(command);
     }
     private static int createClaim(String name, UUID owner, BlockPos min, BlockPos max) {
         ClaimManager.INSTANCE.addClaim(new Claim(name, owner, min, max));
         return 0;
+    }
+    private static int showClaim(ServerPlayerEntity player, Claim claim) {
+        System.out.println(claim);
+        if (claim != null) {
+            for (int x = claim.min.getX(); x < claim.max.getX(); x++) {
+                sendBlockPacket(player, new BlockPos(x, claim.min.getY(), claim.min.getZ()), Blocks.SPONGE.getDefaultState());
+                sendBlockPacket(player, new BlockPos(x, claim.max.getY(), claim.min.getZ()), Blocks.SPONGE.getDefaultState());
+                sendBlockPacket(player, new BlockPos(x, claim.min.getY(), claim.max.getZ()), Blocks.SPONGE.getDefaultState());
+                sendBlockPacket(player, new BlockPos(x, claim.max.getY(), claim.max.getZ()), Blocks.SPONGE.getDefaultState());
+            }
+            for (int y = claim.min.getY(); y < claim.max.getY(); y++) {
+                sendBlockPacket(player, new BlockPos(claim.min.getX(), y, claim.min.getZ()), Blocks.SEA_LANTERN.getDefaultState());
+                sendBlockPacket(player, new BlockPos(claim.max.getX(), y, claim.min.getZ()), Blocks.SEA_LANTERN.getDefaultState());
+                sendBlockPacket(player, new BlockPos(claim.min.getX(), y, claim.max.getZ()), Blocks.SEA_LANTERN.getDefaultState());
+                sendBlockPacket(player, new BlockPos(claim.max.getX(), y, claim.max.getZ()), Blocks.SEA_LANTERN.getDefaultState());
+            }
+            for (int z = claim.min.getZ(); z < claim.max.getZ(); z++) {
+                sendBlockPacket(player, new BlockPos(claim.min.getX(), claim.min.getY(), z), Blocks.REDSTONE_BLOCK.getDefaultState());
+                sendBlockPacket(player, new BlockPos(claim.max.getX(), claim.min.getY(), z), Blocks.REDSTONE_BLOCK.getDefaultState());
+                sendBlockPacket(player, new BlockPos(claim.min.getX(), claim.max.getY(), z), Blocks.REDSTONE_BLOCK.getDefaultState());
+                sendBlockPacket(player, new BlockPos(claim.max.getX(), claim.max.getY(), z), Blocks.REDSTONE_BLOCK.getDefaultState());
+            }
+        }
+        return 0;
+    }
+    private static void sendBlockPacket(ServerPlayerEntity player, BlockPos pos, BlockState state) {
+        BlockUpdateS2CPacket packet =  new BlockUpdateS2CPacket(player.world, pos);
+        if (state != null) ((BlockUpdatePacketMixin)packet).setState(state);
+        player.networkHandler.sendPacket(packet);
     }
 }
---
+++ src/main/java/io/github/indicode/fabric/itsmine/mixin/BlockUpdatePacketMixin.java
@@ -0,0 +1,15 @@
+package io.github.indicode.fabric.itsmine.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.client.network.packet.BlockUpdateS2CPacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+/**
+ * @author Indigo Amann
+ */
+@Mixin(BlockUpdateS2CPacket.class)
+public interface BlockUpdatePacketMixin {
+    @Accessor("state")
+    void setState(BlockState state);
+}
