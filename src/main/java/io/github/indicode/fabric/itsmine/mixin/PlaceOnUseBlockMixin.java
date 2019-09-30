e59a3f4eb7e9e7d87a3f0b471cd89ad4a340e821
Indigo Amann <indigoa.coding@gmail.com>
2019-09-30 01:43:56 -0700

    Fixed lily pads server side

--- src/main/java/io/github/indicode/fabric/itsmine/mixin/PlaceOnUseBlockMixin.java
+++ src/main/java/io/github/indicode/fabric/itsmine/mixin/PlaceOnUseBlockMixin.java
@@ -5,6 +5,7 @@ import io.github.indicode.fabric.itsmine.ClaimManager;
 import io.github.indicode.fabric.itsmine.Functions;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BucketItem;
+import net.minecraft.item.LilyPadItem;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -15,14 +16,22 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 /**
  * @author Indigo Amann
  */
-@Mixin(BucketItem.class)
-public class BucketItemFunctionalityMixin {
+@Mixin({BucketItem.class, LilyPadItem.class})
+public class PlaceOnUseBlockMixin {
     @Redirect(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;canPlayerModifyAt(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/math/BlockPos;)Z"))
     public boolean canActuallyModify(World world, PlayerEntity playerEntity_1, BlockPos blockPos_1) {
         if (!(world instanceof ServerWorld)) return world.canPlayerModifyAt(playerEntity_1, blockPos_1);
         Claim claim = ClaimManager.INSTANCE.getClaimAt(blockPos_1, playerEntity_1.world.getDimension().getType());
         if (claim != null && claim.hasPermissionAt(playerEntity_1.getGameProfile().getId(), Claim.ClaimPermissions.Permission.PLACE_BREAK, blockPos_1))
             return Functions.canPlayerActuallyModifyAt((ServerWorld) world, playerEntity_1, blockPos_1);
-        else return Functions.canModifyAtClaimed(playerEntity_1, blockPos_1);
+        else {
+            if (Functions.canModifyAtClaimed(playerEntity_1, blockPos_1)) return true;
+            else {
+                if ((Object)this instanceof LilyPadItem){
+                    ((ServerWorld)world).method_14178().markForUpdate(blockPos_1);
+                }
+                return false;
+            }
+        }
     }
 }
