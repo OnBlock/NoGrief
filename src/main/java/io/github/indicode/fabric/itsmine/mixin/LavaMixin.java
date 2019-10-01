8568fafd62b60681ea05f537141bb037b34feddf
Indigo Amann <indigoa.coding@gmail.com>
2019-09-30 23:12:47 -0700

    Lava can no longer make fires on different sides of a claim border

--- src/main/java/io/github/indicode/fabric/itsmine/mixin/LavaMixin.java
+++ src/main/java/io/github/indicode/fabric/itsmine/mixin/LavaMixin.java
@@ -3,22 +3,25 @@ package io.github.indicode.fabric.itsmine.mixin;
 import io.github.indicode.fabric.itsmine.Claim;
 import io.github.indicode.fabric.itsmine.ClaimManager;
 import net.minecraft.block.BlockState;
-import net.minecraft.fluid.BaseFluid;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.LavaFluid;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.IWorld;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Random;
 
 /**
  * @author Indigo Amann
  */
-@Mixin(BaseFluid.class)
-public class FluidMixin {
+@Mixin(LavaFluid.class)
+public class LavaMixin {
     @Inject(method = "flow", at = @At("HEAD"), cancellable = true)
     private void dontFlow(IWorld world, BlockPos newPos, BlockState blockState_1, Direction direction, FluidState fluidState_1, CallbackInfo ci) {
         BlockPos oldPos = newPos.offset(direction.getOpposite());
@@ -35,5 +38,22 @@ public class FluidMixin {
                         !(Boolean) newClaim.settings.getSetting(Claim.ClaimSettings.Setting.FLUID_CROSSES_BORDERS)) ci.cancel();
             }
         }
+    }
+    @Redirect(method = "onRandomTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)Z"))
+    public boolean neverGonnaBurnMeUp(World world, BlockPos newPos, BlockState blockState_1, World world_1, BlockPos oldPos, FluidState fluidState_1, Random random_1) {
+        Claim oldClaim = ClaimManager.INSTANCE.getClaimAt(oldPos, world.getDimension().getType());
+        Claim newClaim = ClaimManager.INSTANCE.getClaimAt(newPos, world.getDimension().getType());
+        if (oldClaim != newClaim) {
+            if (oldClaim == null) {
+                if (!(Boolean) newClaim.settings.getSetting(Claim.ClaimSettings.Setting.FLUID_CROSSES_BORDERS)) return false;
+            }
+            else if (newClaim == null) {
+                if (!(Boolean) oldClaim.settings.getSetting(Claim.ClaimSettings.Setting.FLUID_CROSSES_BORDERS)) return false;
+            } else {
+                if (!(Boolean) oldClaim.settings.getSetting(Claim.ClaimSettings.Setting.FLUID_CROSSES_BORDERS) ||
+                        !(Boolean) newClaim.settings.getSetting(Claim.ClaimSettings.Setting.FLUID_CROSSES_BORDERS)) return false;
+            }
+        }
+        return world.setBlockState(newPos, blockState_1);
     }
 }
